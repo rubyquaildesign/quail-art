6f986b421ca98eb242330d5d75a4a21c140f7d21
Ruby Quail <ruby@rubyquail.design>
2025-12-31 22:31:05 +1100

    blah
    Signed-off-by: Ruby Quail <ruby@rubyquail.design>

--- src/clip/clip.ts
+++
@@ -1,5 +0,0 @@
-import { buildClipper } from '../lib/clipping.js';
-export const clipLib = await buildClipper();
-console.log(clipLib);
-
-export default clipLib;
--- src/entry/main.ts
+++ src/entry/main.ts
@@ -1,2 +1,1 @@
 export * from '../lib/index.js';
-export { clipLib } from '../clip/clip.js';
